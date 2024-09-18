3b8d39430399931c6b260a9c9fd580921010e0d5
Varvara.Vorobieva <Varvara.Vorobieva@P-geo.com>
2024-09-18 16:00:03 -0700

    React context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import NavBar from './Components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from "./AuthContext";
 import './App.css';
 import Home from "./Components/Pages/Home";
 import LoginForm from "./Components/Pages/LoginForm";
 import RegisterForm from "./Components/Pages/RegisterForm";
-
+import NavBar from './Components/NavBar';
 
 
 function App() {
---
+++ src/AuthContext.js
@@ -0,0 +1,17 @@
+import React, { useState, createContext } from 'react'; 
+
+//create context object that will allow to both provide and consume state
+const AuthContext = createContext()
+
+//create provider component used to wrap the app and provide state to all children
+export const AuthProvider = ({ children }) => { 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <AuthContext.Provider value = {{ isLoggedIn, setIsLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
